5d278fe9ab296d9d5f114185c5beb2db2638e951
SeRgNeschetniy <xdsergx@gmail.com>
2023-05-25 23:35:09 +0300

    add create telegram bot

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log("working");
+import * as dotenv from "dotenv";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+
+dotenv.config();
+
+const { BOT_TOKEN } = process.env;
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.on(message("voice"), async (ctx) => {
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    console.log(link.href);
+    await ctx.reply(JSON.stringify(link, null, 2));
+  } catch (error) {
+    console.log(`Error while voice message`, e.message);
+  }
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
